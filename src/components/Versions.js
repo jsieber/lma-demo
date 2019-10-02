a1a015f7590d0bbd05ed3d3cf182d28979d2c437
John Sieber <jsieber@adventurecycling.org>
2019-10-01 23:12:01 -0600

    version listing

--- src/components/Versions.js
+++ src/components/Versions.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { Link } from "react-router-dom";
 import axios from 'axios';
+import '../App.css';
+import ListGroup from 'react-bootstrap/ListGroup';
 
-class Versions extends React.Component {
+class fetchVersions extends React.Component {
   constructor(props) {
     super(props);
     //console.log(this.props.match.params.identifier);
@@ -14,19 +16,18 @@ class Versions extends React.Component {
   }
 
   componentDidMount() {
-    axios.get(`https://archive.org/advancedsearch.php?q=collection%3AJoeRussosAlmostDead+date%3A${this.props.match.params.date}&fl%5B%5D=collection&fl%5B%5D=date&fl%5B%5D=downloads&sort%5B%5D=&sort%5B%5D=&sort%5B%5D=&rows=50&page=1&output=json`)
+    axios.get(`https://archive.org/advancedsearch.php?q=collection%3AJoeRussosAlmostDead+date%3A${this.props.match.params.date}&fl%5B%5D=avg_rating&fl%5B%5D=collection&fl%5B%5D=contributor&fl%5B%5D=date&fl%5B%5D=downloads&fl%5B%5D=format&fl%5B%5D=headerImage&fl%5B%5D=identifier&fl%5B%5D=publisher&fl%5B%5D=source&fl%5B%5D=title&sort%5B%5D=&sort%5B%5D=&sort%5B%5D=&rows=50&page=1&output=json`)
       .then(res => {
-        console.log(res.data.response.docs);
+        //console.log(res.data.response.docs);
         //console.log(res.data.files);
         //console.log(res.data.metadata.title);
         // Update state to trigger a re-render.
         // Clear any errors, and turn off the loading indiciator.
         //this.setState({shows: res.data.response.docs});
-        //this.setState({showDetails: res.data});
+        this.setState({shows: res.data.response.docs});
 
         this.setState({
           error: null,
-
           loading: false,
         });
 
@@ -53,19 +54,26 @@ class Versions extends React.Component {
     );
   }
 
-  renderShows() {
+  renderVersions() {
 
     if(this.state.error) {
       return this.renderError();
     }
-    console.log("");
+    console.log(this.state.shows);
     //console.log(this.state.playlist);
     //const theplaylist = this.state.playlist;
     //console.log(theplaylist);
     return (
 
       <div>
+          <ListGroup variant="flush">
+              {this.state.shows.map(s =>
 
+                <ListGroup.Item action variant="light" key={s.identifier} className="showList">
+                  <Link to={`/show/${s.identifier}`}>{s.title} - Downloads {s.downloads}</Link>
+                </ListGroup.Item>
+              )}
+          </ListGroup>
 
       </div>
     );
@@ -78,11 +86,11 @@ class Versions extends React.Component {
         <h1>{this.props.collection}</h1>
         {this.state.loading ?
           this.renderLoading()
-          : this.renderShows()}
+          : this.renderVersions()}
       </div>
     );
   }
 
 }
 
-export default Versions;
+export default fetchVersions;
